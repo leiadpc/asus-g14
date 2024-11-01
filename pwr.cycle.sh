d194db4362872978973de560702d01b89f85af6a
leiadpc <50549830+leiadpc@users.noreply.github.com>
2024-11-01 18:58:26 -0400

    Update pwr.cycle.sh
    Added commands to set system power profiles as well

--- pwr.cycle.sh
+++ pwr.cycle.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# Dependencies: asusctl, powerprofilesctl, notify-send
 asusctl profile -n
-power=$(asusctl profile --profile-get)
-notify-send -t 700 -u normal -i video-display "$power"
+power=$(asusctl profile --profile-get | awk '{print $4}')
+if [[ $power == "Quiet" ]]; then
+  powerprofilesctl set power-saver
+elif [[ $power == "Balanced" ]]; then
+    powerprofilesctl set balanced
+elif [[ $power == "Performance" ]]; then
+    powerprofilesctl set performance
+fi
+notify-send  -a asusctl -t 1200 -u normal -i video-display "Power profile set to $power"
