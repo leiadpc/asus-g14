60844a6b6a10d686c69c504ac51210fc7119155c
leiadpc <50549830+leiadpc@users.noreply.github.com>
2024-11-01 19:07:24 -0400

    Delete touchpad_toggle.sh

--- touchpad_toggle.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-status=$(gsettings get org.gnome.desktop.peripherals.touchpad disable-while-typing)
-if [ "$status" == "true" ]
-then gsettings set org.gnome.desktop.peripherals.touchpad disable-while-typing false && notify-send --icon=input-touchpad-symbolic --app-name="Touchpad Toggle" "Touchpad enabled while typing" 
-elif [ "$status" != "true" ]
-then gsettings set org.gnome.desktop.peripherals.touchpad disable-while-typing true && notify-send --icon=touchpad-disabled-symbolic --app-name="Touchpad Toggle" "Touchpad disabled while typing"
-fi
-
