0550db3f3f86a22d37837d34d79a9214a370b6df
leiadpc <50549830+leiadpc@users.noreply.github.com>
2024-11-02 08:52:47 -0400

    Update pwr.cycle.sh
    Added section to check if powerprofilesctl needs set or not

--- pwr.cycle.sh
+++ pwr.cycle.sh
@@ -2,12 +2,13 @@
 
 # Dependencies: asusctl, powerprofilesctl, notify-send
 asusctl profile -n
+syspwr=$(powerprofilesctl get)
 power=$(asusctl profile --profile-get | awk '{print $4}')
-if [[ $power == "Quiet" ]]; then
+if [ $power == "Quiet" -a $syspwr != "power-saver" ]; then
   powerprofilesctl set power-saver
-elif [[ $power == "Balanced" ]]; then
+elif [ $power == "Balanced" -a $syspwr != "balanced" ]; then
     powerprofilesctl set balanced
-elif [[ $power == "Performance" ]]; then
+elif [ $power == "Performance" -a $syspwr != "performance" ]; then
     powerprofilesctl set performance
 fi
 notify-send  -a asusctl -t 1200 -u normal -i video-display "Power profile set to $power"
